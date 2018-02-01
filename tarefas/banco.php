99e26877fbe617fbc7e362f91eba500bea28f2b8
fabio <fabiodeoliveira07@gmail.com>
2018-02-01 01:43:08 -0200

    removendo bug que exclui todos os arquivos

--- tarefas/banco.php
+++ tarefas/banco.php
@@ -69,12 +69,13 @@ function remover_tarefa($conexao, $id) {
 }
 
 function remover_anexo($conexao, $id) {
-	$sqlBuscarNome = "SELECT nome FROM anexos WHERE tarefa_id = {$id}";
+	$sqlBuscarNome = "SELECT nome FROM anexos WHERE id = {$id}";
+
 	$resultado =  mysqli_query($conexao, $sqlBuscarNome);
 	$resultado = mysqli_fetch_assoc($resultado);
 	unlink("anexos/{$resultado['nome']}");
 
-	$sqlRemover = "DELETE FROM anexos WHERE tarefa_id = {$id}";
+	$sqlRemover = "DELETE FROM anexos WHERE id = {$id}";
 
 	mysqli_query($conexao, $sqlRemover);
 
--- tarefas/remover_arquivo.php
+++ tarefas/remover_arquivo.php
@@ -1,7 +1,5 @@
 <?php
 
 include_once("banco.php");
-
 remover_anexo($conexao, $_GET['id']);
-
-header("Location: tarefa.php?id={$_GET['id']}");
+header("Location: tarefa.php?id={$_GET['tarefa_id']}");
