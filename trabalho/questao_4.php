1b105c72ed8cd8cff693768ef1844b9575aadca2
Fabio de Oliveira <fabiodeoliveira07@gmail.com>
2018-02-02 23:35:40 -0200

    insercao de comentarios

--- trabalho/questao_4.php
+++ trabalho/questao_4.php
@@ -1,18 +1,18 @@
 <?php 
-
-
 function calculaExpressoesEmString($expressao) {
+	// Verifica se a expressao e valida
 	$padrao =  '/^(\d+[-+\/x])+\d+$/';
 	$resultado = preg_match($padrao, $expressao);
 
 	if (! $resultado) {
 		return "Expressão invalida foi encontrada na execução";
 	}
-
+	// Separa os numeros e os sinais em um array
 	preg_match_all("/\d+|-|\+|\/|x/", $expressao, $equacao);
 	$equacao = $equacao[0];
-	$soma = intval($equacao[0]);
 
+	// Efetua os calculos
+	$soma = intval($equacao[0]);
 	foreach ($equacao as $key => $value) {
 		if ($key % 2 == 0) {
 			continue;
